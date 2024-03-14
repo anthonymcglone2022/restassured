43367519eddb4626415ce4a91c666e0948c37081
Anthony McGlone <102866938+anthonymcglone2022@users.noreply.github.com>
2024-03-14 11:56:14 +0000

    Update Config.java

--- src/test/java/util/Config.java
+++ src/test/java/util/Config.java
@@ -1,10 +1,10 @@
 package util;
 
 public class Config {
-	public static String baseURL = "http://127.0.0.1:9191/";
-	public static String createPostCode = "createPostCode/";
-	public static String formatPostCode = "formatPostCode/";
-	public static String contentTypeJson = "application/json";
-	
-	public static int twoHundred = 200;
+    public static String baseURL = "http://127.0.0.1:9191/";
+    public static String createPostCode = "createPostCode/";
+    public static String formatPostCode = "formatPostCode/";
+    public static String contentTypeJson = "application/json";
+
+    public static int twoHundred = 200;
 }
